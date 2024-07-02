3ae69494f4e3ad29bbf303dadcc6de433e8b6eb1
saeed <saidqtaish@gmail.com>
2024-07-02 16:50:08 +0300

    adding login & registration assignment

---
+++ LoginAndRegistration/src/main/java/com/codingdojo/loginandregistration/controllers/HomeController.java
@@ -0,0 +1,78 @@
+package com.codingdojo.loginandregistration.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.codingdojo.loginandregistration.models.LoginUser;
+import com.codingdojo.loginandregistration.models.User;
+import com.codingdojo.loginandregistration.services.UserService;
+
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+
+@Controller
+public class HomeController {
+	@Autowired
+	private UserService userServ;
+
+	@GetMapping("/")
+	public String index(@ModelAttribute("user") User newUser  ,
+			@ModelAttribute("newLogin") User User)
+	{
+		return "register.jsp";
+	}
+	
+	
+	
+	@PostMapping("/register")
+	public String register(@Valid @ModelAttribute("user") User newUser, 
+	                       BindingResult result, Model model, HttpSession session) {
+
+	    if (result.hasErrors()) {
+	        model.addAttribute("newLogin", new User()); 
+	        return "register.jsp";
+	    }
+	    User user= userServ.register(newUser, result);
+	    return "redirect:/"; 
+	}
+
+
+	@PostMapping("/login")
+	public String login(@Valid @ModelAttribute("newLogin") LoginUser newLogin, 
+	                    BindingResult result, Model model, HttpSession session) {
+		User user = userServ.login(newLogin, result);
+	    if (result.hasErrors()) {
+            model.addAttribute("user", new User());
+	        return "register.jsp"; 
+	    }
+
+	    session.setAttribute("userid", user.getId());
+	    session.setAttribute("name", user.getUserName());
+	    
+	    return "redirect:/home";
+		
+	}
+
+	@GetMapping("/home")
+	public String home(HttpSession session) {
+		if (session == null) {
+			return "redirect:/"; 
+	}
+		else {
+			return "welcome.jsp";
+		}
+	}
+	
+	@GetMapping("/logout")
+	public String logout(HttpSession session) {
+		session.invalidate();
+		return "redirect:/";
+	}
+}
+
+
---
+++ LoginAndRegistration/src/main/java/com/codingdojo/loginandregistration/services/UserService.java
@@ -0,0 +1,51 @@
+package com.codingdojo.loginandregistration.services;
+
+import java.util.Optional;
+
+import org.mindrot.jbcrypt.BCrypt;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.BindingResult;
+import com.codingdojo.loginandregistration.models.LoginUser;
+import com.codingdojo.loginandregistration.models.User;
+import com.codingdojo.loginandregistration.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository userRepo;
+
+	public User register(User newUser, BindingResult result) {
+		String email = newUser.getEmail();
+		Optional<User> optionalUser= userRepo.findByEmail(email);
+		if (optionalUser.isPresent()) {
+			result.rejectValue("email", "Matches", "Email already exists!");
+			return null;
+		}
+		if (!newUser.getPassword().equals(newUser.getConfirm())) {
+			result.rejectValue("confirm", "Matches", "The Confirm Password must match Password!");
+			return null;
+		}
+		String hashed = BCrypt.hashpw(newUser.getPassword(), BCrypt.gensalt());
+		newUser.setPassword(hashed);
+		return userRepo.save(newUser);
+	}
+
+	public User login(LoginUser newLogin, BindingResult result) {
+	    Optional<User> potentialUser = userRepo.findByEmail(newLogin.getEmail());
+	    
+	    if (!potentialUser.isPresent()) {
+	        result.rejectValue("email", "NotFound", "User not found for this email address");
+	        return null;
+	    }
+	    
+	    User user = potentialUser.get();
+	    
+	    if(!BCrypt.checkpw(newLogin.getPassword(), user.getPassword())) {
+	        result.rejectValue("password", "Matches", "Invalid Password!");
+	    }
+	    
+	    return user;
+	}
+}
